a98fc9d3c346ea7528d476dda6170f9343be2817
Nicholas Karsant <nkarsant@gmail.com>
2021-02-03 12:38:51 -0800

    add Redux form and submit handler

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Field, reduxForm } from "redux-form";
 
+class StreamCreate extends React.Component {
+  renderInput({ input, label }) {
+    return (
+      // <input
+      // onChange={formProps.input.onChange}
+      // value={formProps.input.value}
+      // />
+      // above input is equal to below input
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
 
-const StreamCreate = () => {
+  onSubmit(formValues) {
 
+  }
 
-  return <div>StreamCreate</div>
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+        <Field name="title" label="Enter Title" component={this.renderInput} />
+        <Field
+          name="description"
+          label="Enter Description"
+          component={this.renderInput}
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
 }
 
-
-export default StreamCreate;
+export default reduxForm({ form: "streamCreate" })(StreamCreate);
