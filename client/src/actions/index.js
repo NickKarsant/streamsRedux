e3060cc3f97413b51166a4fd6d73738502fa59ef
Nicholas Karsant <nkarsant@gmail.com>
2021-02-05 20:01:59 -0800

    add programmatic navigation after create stream form submitted

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,6 +8,8 @@ import {
   DELETE_STREAM
 } from "./types";
 import streams from "../apis/streams";
+import history from '../history';
+
 
 export const signIn = userId => {
   return {
@@ -28,6 +30,9 @@ export const createStream = formValues  => {
     const response = await streams.post("/streams", {...formValues, userId});
 
     dispatch({ type: CREATE_STREAM, payload: response.data });
+
+    // programmatic navigation to return user to root route
+    history.push('/');
   };
 };
 
