a31eac0ae467e90dfcfbfe7a168e059f25a4b133
Nicholas Karsant <nkarsant@gmail.com>
2021-02-12 14:36:31 -0800

    add modal for delete page

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import Modal from "../Modal";
+import history from "../../history";
+import StreamForm from "./StreamForm";
+
 
 
 const StreamDelete = () => {
 
+  const actions = (
+    <React.Fragment>
+      <button className="ui negative button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
 
-  return <div>StreamDelete</div>
-}
-
+  return (
+    <div>
+      StreamDelete
+      <StreamForm />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push('/streams/delete')}
+      />
+    </div>
+  );
+};
 
 export default StreamDelete;
