023125d3e7d689f3f5d2fdbafe84537ab9014020
Nicholas Karsant <nkarsant@gmail.com>
2021-02-12 12:43:50 -0800

    patch for edit path

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,25 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
-  render() {
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
 
-    if (!this.props.stream){
+  render() {
+    if (!this.props.stream) {
       return <div>Loading...</div>;
-    } 
+    }
 
     return (
       <div>
         <h2>Edit Stream</h2>
-        <form>
-
-          <button className="ui button primary">Submit</button>
-        </form>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          initialValues={{
+            title: this.props.stream.title,
+            description: this.props.stream.description
+          }}
+        />
       </div>
     );
   }
@@ -29,4 +36,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
