56cb0d4fd2845e6277f1d1db161c939d9f7a95e7
Nicholas Karsant <nkarsant@gmail.com>
2021-02-12 22:21:03 -0800

    show page without video player

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
 
-const StreamShow = () => {
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
 
 
-  return <div>StreamShow</div>
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        {/* <ReactPlayer url=""/> */}
+        <h1 className= "header">{this.props.stream.title}</h1>
+        <h5 className= "content">{this.props.stream.description}</h5>
+      </div>
+    )
+  }
 }
 
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
+
